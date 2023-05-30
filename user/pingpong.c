be5fc4332f9c3c933256c8384d827a1f795d2204
zefeiy <1241808529@qq.com>
2023-05-30 15:59:14 +0800

    up

--- user/pingpong.c
+++ user/pingpong.c
@@ -11,17 +11,13 @@ int main (int argc, char* argv) {
     pipe(p2);
     int pid = fork();
     if (pid > 0) {
-	char buf = 't';
-       close(p1[0]);
-       close(p2[1]);
-       if( write(p1[1], &buf, 1) != 1) {
-	   fprintf(1, "Usage: write failed...\n");
-	   exit(1);
-        }
-       
-       if( read(p2[0], &buf, 1) == 1 ) {
-           fprintf(1, "%d :received pong\n", getpid());
-       }
+	    char* buf = "ping";
+        close(p1[0]);
+        close(p2[1]);
+        write(p1[1], buf, 5);
+        read(p2[0], buf, 5);
+        fprintf(1, "%d :received %s\n", getpid(), buf);
+
        int i = 0;
        wait(&i);
        close(p1[1]);
@@ -31,13 +27,13 @@ int main (int argc, char* argv) {
     else {
         close(p1[1]);
 	    close(p2[0]);
-	    char buf = 't';
-            read(p1[0], &buf, 1);
-	    fprintf(1, "%d :received ping\n", getpid());
-	    write(p2[1], &buf, 1);
+	    char* buf = 'pong';
+            read(p1[0], buf, 5);
+	    fprintf(1, "%d :received %s\n", getpid(), buf);
+	    write(p2[1], buf, 5);
 	    close(p1[0]);
 	    close(p2[1]);
 	    exit(1);
-         }	
+    }	
     exit(1);
 }
